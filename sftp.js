4ab088d3a9c46da39b6c4df1f2162f2c1be425c9
manan <mananchawla10@gmail.com>
2023-07-04 19:22:27 +0530

    a

--- sftp.js
+++ sftp.js
@@ -1,70 +1,17 @@
 import fs from 'fs';
 import path from 'path';
 import SftpClient from 'ssh2-sftp-client';
+import dotenv from 'dotenv';
 
-async function saveFileToServer(localFilePath1, fileName, serverConfig) {
-    
-    const serverConfig2 = {
-        host: '192.168.122.162',
-        username: 'sftpuser',
-        password: 'sftp',
-        remotePath: '', // Optional
-    };
-    let localFolderPath = './imgs';
-    console.log(serverConfig2);
-    // if (!serverConfig || !serverConfig.host || !serverConfig.username || !serverConfig.password) {
-    //     throw new Error('Incomplete server configuration.');
-    // }
+dotenv.config();
 
-    const sftp = new SftpClient();
-
-    try {
-        await sftp.connect(serverConfig2);
-        console.log('Connected to server.');
-        const files = await getFilesInFolder(localFolderPath);
-        const uploadPromises = files.map(async (file) => {
-            const localFilePath = path.join(localFolderPath, file);
-            const serverPath = `${serverConfig2.remotePath || '/sftpuser'}/${file}`;
-            await sftp.fastPut(localFilePath, serverPath);
-
-
-            // const loggerData = {
-            //     loginStatus: 'SUCCESS',
-            //     serverIP: serverConfig.host,
-            //     userName: serverConfig.username,
-            //     password: serverConfig.password,
-            //     serverPath: serverConfig.remotePath,
-            //     type: 'PUT',
-            //     fileName: fileName,
-            // };
-            // console.log(JSON.stringify(loggerData));
-
-            return { response: true, nwcServerPath: serverPath, fileName: file };
-        });
-        const results = await Promise.all(uploadPromises);
-
-        return results;
-
-    } catch (err) {
-
-        // const loggerData = {
-        //     loginStatus: 'FAILED',
-        //     serverIP: serverConfig.host,
-        //     userName: serverConfig.username,
-        //     password: serverConfig.password,
-        //     serverPath: serverConfig.remotePath,
-        //     type: 'PUT',
-        //     fileName: fileName,
-        //     error: err.message || 'Unknown error occurred.',
-        // };
-        // console.error(JSON.stringify(loggerData));
-        //
-        return { response: false, nwcServerPath: '', fileName: fileName, error: err.message || 'Unknown error occurred.' };
-    } finally {
-        sftp.end();
-    }
-}
-
+// TODO: Add your server configuration .env
+// const serverConfig = {
+//     host: process.env.SFTP_HOST,
+//     username: process.env.SFTP_USER,
+//     password: process.env.SFTP_PASSWORD,
+//     remotePath: process.env.SFTP_REMOTE_DIR,
+// }
 
 const serverConfig = {
     host: '192.168.122.162',
@@ -72,21 +19,102 @@ const serverConfig = {
     password: 'sftp',
     remotePath: '', // Optional
 };
-const localFolderPath = 'D:/web-development/wp_clone';
 
-// const localFilePath = 'D:\web-development\wp_clone';
-// const fileName = 'test.txt';
-
-function getFilesInFolder(localFolderPath) {
-    return fs.promises.readdir(localFolderPath);
-}
+const localFolderPath = './imgs';
 
 (async () => {
     try {
-        console.log(serverConfig)
         const results = await saveFileToServer(localFolderPath, serverConfig);
         console.log('Upload results:', results);
     } catch (error) {
         console.error('Error occurred during upload:', error);
     }
 })();
+
+function getFilesInFolder(localFolderPath) {
+    return fs.promises.readdir(localFolderPath);
+}
+
+async function saveFileToServer(localFilePath, serverConfig) {
+    if (!serverConfig || !serverConfig.host || !serverConfig.username || !serverConfig.password) {
+        throw new Error('Incomplete server configuration.');
+    }
+
+    const sftp = new SftpClient();
+    const loggerData = {
+        loginStatus: 'SUCCESS',
+        serverIP: serverConfig.host,
+        userName: serverConfig.username,
+        password: serverConfig.password,
+        serverPath: serverConfig.remotePath,
+        type: 'PUT',
+    };
+    const loggerDetailsFolder = './logs';
+    const loggerFilePath = path.join(loggerDetailsFolder, 'logger_details');
+    let filesInServer = []
+    try {
+        await sftp.connect(serverConfig);
+        console.log('Connected to server.');
+        const result = await sftp.list(serverConfig.remotePath || '/sftpuser');
+        for (const sub of result) {
+            filesInServer.push(sub.name)
+        }
+        let files = await getFilesInFolder(localFolderPath);
+        files = files.filter(file => !filesInServer.includes(file))
+        const uploadPromises = files.map(async (file) => {
+            const localFilePath = path.join(localFolderPath, file);
+            const serverPath = `${serverConfig.remotePath || '/sftpuser'}/${file}`;
+            await sftp.put(localFilePath, serverPath);
+
+            console.log(JSON.stringify(loggerData));
+
+            return { response: true, serverPath: serverPath, fileName: file };
+        });
+        const results = await Promise.all(uploadPromises);
+        // Create the "loggerDetails" folder if it doesn't exist
+        if (!fs.existsSync(loggerDetailsFolder)) {
+            fs.mkdirSync(loggerDetailsFolder);
+        }
+
+        // Create the "logger_details" file if it doesn't exist
+        if (!fs.existsSync(loggerFilePath)) {
+            fs.writeFileSync(loggerFilePath, '');
+        }
+
+        // Write logger data to the file
+        fs.writeFileSync(loggerFilePath, JSON.stringify(loggerData, null, 2));
+
+
+        return results;
+
+    } catch (err) {
+        const loggerDataError = {
+            loginStatus: 'FAILED',
+            serverIP: serverConfig.host,
+            userName: serverConfig.username,
+            password: serverConfig.password,
+            serverPath: serverConfig.remotePath,
+            type: 'PUT',
+            error: err.message || 'Unknown error occurred.',
+        };
+
+        // Create the "loggerDetails" folder if it doesn't exist
+        if (!fs.existsSync(loggerDetailsFolder)) {
+            fs.mkdirSync(loggerDetailsFolder);
+        }
+
+        // Create the "logger_details" file if it doesn't exist
+        if (!fs.existsSync(loggerFilePath)) {
+            fs.writeFileSync(loggerFilePath, '');
+        }
+
+        // Write error logger data to the file
+        fs.writeFileSync(loggerFilePath, JSON.stringify(loggerDataError, null, 2));
+
+        return { response: false,serverPath: '', error: err.message || 'Unknown error occurred.' };
+    } finally {
+        sftp.end();
+    }
+}
+
+
